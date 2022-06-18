dcb75cbc2f1c8f36f44a78ffe03552abfa27adb2
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-18 13:38:15 -0700

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
