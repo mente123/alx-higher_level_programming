4514870ecdc65567506384f0141e70db1a81addc
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-18 12:58:00 -0700

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status
-using requests"""
-if __name__ == "__main__":
-    import requests
-
-    r = requests.get("https://alx-intranet.hbtn.io/status")
-    print('Body response:\n\t- type: {}\n\t- content: {}'
-          .format(type(r.text), r.text))
