a08f5891daa6cd13c7218cb7cb16e95fcb9570d4
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-18 12:49:57 -0700

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
-curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
