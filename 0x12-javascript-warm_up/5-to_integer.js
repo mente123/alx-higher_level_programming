fad4ab5bc2357a3a235f48366586b2c0dcfcdd11
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-22 04:23:04 -0400

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const argv = process.argv.slice(2);
+if (isNaN(parseInt(argv[0]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(argv[0]));
+}
