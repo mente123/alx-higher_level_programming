3ce3d9b28d393b146ca057a9d02733b1b15ee61f
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-22 04:30:42 -0400

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const argv = process.argv.slice(2);
+if (!argv[0] || argv.length === 1) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  const descSort = argv.sort((a, b) => b - a);
+  console.log(descSort[1]);
 }
