33453a5e25a9d1234f42b816b2a479649374bfb5
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-22 04:19:53 -0400

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+const argv = process.argv.slice(2);
+if (!argv[0]) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+  console.log(argv[0]);
 }
